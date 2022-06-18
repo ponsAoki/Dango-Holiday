cfd404a140f8972d6b7921240af9925a3f8b1847
ponsAoki <aokishogo20pk@gmail.com>
2022-06-18 16:22:34 +0900

    add events

--- src/components/HelloWorld2.vue
+++ src/components/HelloWorld2.vue
@@ -56,9 +56,10 @@
       <v-calendar
         ref="calendar"
         v-model="value"
+        :events="holidays"
+        :event-color="getHolidayColor"
         :type="type"
         locale="ja-jp"
-        @change="getEvents"
       ></v-calendar>
     </v-sheet>
   </div>
@@ -76,17 +77,17 @@ export default {
 
     value: moment().format("yyyy-MM-DD"), // 現在日時
 
-    type: "month",
-    types: ["month", "week", "day", "4day"],
-    mode: "stack",
-    modes: ["stack", "column"],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { text: "Sun - Sat", value: [0, 1, 2, 3, 4, 5, 6] },
-      { text: "Mon - Sun", value: [1, 2, 3, 4, 5, 6, 0] },
-      { text: "Mon - Fri", value: [1, 2, 3, 4, 5] },
-      { text: "Mon, Wed, Fri", value: [1, 3, 5] },
-    ],
+    // type: "month",
+    // types: ["month", "week", "day", "4day"],
+    // mode: "stack",
+    // modes: ["stack", "column"],
+    // weekday: [0, 1, 2, 3, 4, 5, 6],
+    // weekdays: [
+    //   { text: "Sun - Sat", value: [0, 1, 2, 3, 4, 5, 6] },
+    //   { text: "Mon - Sun", value: [1, 2, 3, 4, 5, 6, 0] },
+    //   { text: "Mon - Fri", value: [1, 2, 3, 4, 5] },
+    //   { text: "Mon, Wed, Fri", value: [1, 3, 5] },
+    // ],
     events: [],
     colors: [
       "blue",
@@ -97,19 +98,25 @@ export default {
       "orange",
       "grey darken-1",
     ],
-    names: [
-      "Meeting",
-      "Holiday",
-      "PTO",
-      "Travel",
-      "Event",
-      "Birthday",
-      "Conference",
-      "Party",
-    ],
+    // names: [
+    //   "Meeting",
+    //   "Holiday",
+    //   "PTO",
+    //   "Travel",
+    //   "Event",
+    //   "Birthday",
+    //   "Conference",
+    //   "Party",
+    // ],
   }),
 
+  mounted() {
+    //試しに5/3~5/6まで休みにしてみる
+    this.getHoliday();
+  },
+
   computed: {
+    //読み込み時に現在の日付を取得
     title() {
       return moment(this.value).format("yyyy年 M月"); // 表示用文字列を返す
     },
@@ -120,39 +127,52 @@ export default {
       console.log(this.year);
       return this.year;
     },
-
-    getEvents({ start, end }) {
-      const events = [];
-
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        });
-      }
-
-      this.events = events;
+    getHoliday() {
+      const holidays = [
+        {
+          name: "休み",
+          start: moment("2022-05-03").toDate(),
+          end: moment("2022-05-06").toDate(),
+          color: "blue",
+        },
+      ];
+      this.holidays = holidays;
+      return this.holiday;
     },
-    getEventColor(event) {
-      return event.color;
+
+    // getEvents({ start, end }) {
+    //   const events = [];
+
+    //   const min = new Date(`${start.date}T00:00:00`);
+    //   const max = new Date(`${end.date}T23:59:59`);
+    //   const days = (max.getTime() - min.getTime()) / 86400000;
+    //   const eventCount = this.rnd(days, days + 20);
+
+    //   for (let i = 0; i < eventCount; i++) {
+    //     const allDay = this.rnd(0, 3) === 0;
+    //     const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+    //     const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+    //     const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+    //     const second = new Date(first.getTime() + secondTimestamp);
+
+    //     events.push({
+    //       name: this.names[this.rnd(0, this.names.length - 1)],
+    //       start: first,
+    //       end: second,
+    //       color: this.colors[this.rnd(0, this.colors.length - 1)],
+    //       timed: !allDay,
+    //     });
+    //   }
+
+    //   this.events = events;
+    // },
+    //「休み」の日を色付け
+    getHolidayColor(holiday) {
+      return holiday.color;
     },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
+    // rnd(a, b) {
+    //   return Math.floor((b - a + 1) * Math.random()) + a;
+    // },
   },
 };
 </script>
