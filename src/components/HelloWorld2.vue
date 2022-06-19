901e87c5b24511b8e39fd391001af37deba94316
ponsAoki <aokishogo20pk@gmail.com>
2022-06-19 14:07:58 +0900

    HW2 編集

--- src/components/HelloWorld2.vue
+++ src/components/HelloWorld2.vue
@@ -14,6 +14,9 @@
         </v-autocomplete>
       </v-col>
     </v-sheet>
+    <v-alert border="top" color="blue lighten-2" dark
+      >GWまで、あと{{ countdown }}日</v-alert
+    >
     <v-divider></v-divider>
     <!-- ここからカレンダー -->
     <v-sheet tile height="54" class="d-flex">
@@ -29,7 +32,7 @@
       <v-calendar
         ref="calendar"
         v-model="value"
-        :events="holidays"
+        :events="holidayevents"
         :event-color="getVacationColor"
         locale="ja-jp"
       ></v-calendar>
@@ -39,9 +42,9 @@
 
 <script>
 import moment from "moment";
-import { getHoliday } from "../logic";
+import { getHoliday } from "../logic copy";
 // import { getDayOfWeekOnHoliday } from "../logic";
-import { getDaysOfWeekOnHolidayLike } from "../logic";
+// import { getDaysOfWeekOnHolidayLike } from "../logic";
 
 export default {
   name: "HelloWorld2",
@@ -50,10 +53,13 @@ export default {
     years: [],
     year: "",
 
+    countdown: 365,
+
     today: moment().format("yyyy-MM-DD"),
     value: moment().format("yyyy-MM-DD"), // 現在日時
 
     holidays: [],
+    holidayevents: [],
     colors: [
       "blue",
       "indigo",
@@ -72,7 +78,7 @@ export default {
     this.year = this.value.split("-")[0];
     const y = parseInt(this.year);
     this.years.push(y - 2, y - 1, y, y + 1);
-    getDaysOfWeekOnHolidayLike();
+    // getHoliday();
     // const genzai = new Date();
     // console.log(genzai.getDay());
   },
@@ -80,7 +86,7 @@ export default {
   //初回読み込み時にyearに値が入ると実行
   watch: {
     year() {
-      this.holidayLen = getHoliday(this.year);
+      this.holidays = getHoliday(this.year);
       this.getVacation();
     },
   },
@@ -96,8 +102,8 @@ export default {
   methods: {
     //yearを初回から変更したとき
     yearChange() {
-      this.holidayLen = getHoliday(this.year);
-      console.log(this.holidayLen);
+      this.holidays = getHoliday(this.year);
+      console.log(this.holidays);
       this.value = `${this.year}` + "-05-01";
       this.getVacation();
     },
@@ -115,15 +121,13 @@ export default {
         //   color: "blue",
         // },
         {
-          name: `${this.holidayLen}連休`,
-          start: moment(`${this.year}` + "-05-03").toDate(),
-          end: moment(
-            `${this.year}-05-${3 + parseInt(this.holidayLen) - 1}`
-          ).toDate(),
+          name: "連休",
+          start: moment(this.holidays[1]).toDate(),
+          end: moment(this.holidays[3]).toDate(),
           color: "blue",
         },
       ];
-      this.holidays = holidays;
+      this.holidayevents = holidays;
       return this.holiday;
     },
 
